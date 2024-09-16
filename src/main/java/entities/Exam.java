011820da95584bcbaf55e79a85745dd33fb72e45
kassu11 <61124141+kassu11@users.noreply.github.com>
2024-09-16 10:06:16 +0300

    ORN exercise

---
+++ src/main/java/entities/Exam.java
@@ -0,0 +1,51 @@
+package entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "exams")
+public class Exam {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @Column(name = "grade", nullable = false)
+    private int grade; // Grades will be on a scale of 1-5
+
+    public Exam() {}
+
+    public Exam(Student student, int grade) {
+        this.student = student;
+        this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}
